dfdf83acc7970c4a7f81a4a8bd19e024682b145f
Marko Pe <marko.petzold@record-evolution.de>
2023-10-15 11:32:52 +0200

    working interactively

--- index.py
+++ index.py
@@ -1,23 +1,75 @@
-import asyncio
-from reswarm import Reswarm
+from ultralytics import YOLO
+# from reswarm import Reswarm
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import cv2
+import math 
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
-# create a reswarm instance, which auto connects to the Record Evolution Platform
-# the reswarm instance handles authentication and reconnects when connection is lost
-rw = Reswarm()
+# model
+model = YOLO("./yolov8l.pt")
 
-async def main():
-    """Publishes sample data every 2 seconds to the 're.hello.world' topic
-    """
-    while True:
-        data = {"temperature": 20}
-        # publish an event (if connection is not established the publish is skipped)
-        await rw.publish('re.hello.world', data)
-        print(f'Published {data} to topic re.hello.world')
-        await asyncio.sleep(2)
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
+# framerate = 25.0
 
-if __name__ == "__main__":
-    # run the main coroutine
-    asyncio.get_event_loop().create_task(main())
-    # run the reswarm component
-    rw.run()
+out = cv2.VideoWriter('appsrc ! videoconvert ! '
+                      'vp8enc deadline=2 threads=2 keyframe-max-dist=60 ! video/x-vp8 ! '
+                      'rtpvp8pay !'
+                      'udpsink host=127.0.0.1 port=5004',
+                      0, framerate, (640, 480))
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+    
+    # coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
