99e021c4f9fbc0efd9f0f00a69112976e931f7e4
Marko Petzold <marko.petzold@record-evolution.de>
2023-11-25 22:28:50 +0100

    hu

--- web/src/camera-shell.ts
+++ web/src/camera-shell.ts
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import './camera-player.js'
-import { initJanus } from './modules/webRTCPlayer.js'
+import { initJanus } from './modules/webRTCPlayer'
 
 @customElement('camera-shell')
 export class CameraShell extends LitElement {
