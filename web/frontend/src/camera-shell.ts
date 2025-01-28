f21714da96347542a3a918c9b3765d46a0d6794c
marko <marko.petzold@record-evolution.de>
2025-01-28 19:35:59 +0100

    wip

--- web/frontend/src/camera-shell.ts
+++ web/frontend/src/camera-shell.ts
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import './camera-player.js';
-import { initJanus } from './modules/webRTCPlayer.js';
+// import { initJanus } from './modules/webRTCPlayer.js';
 import { CameraPlayer } from './camera-player.js';
 import { mainStyles } from './utils.js';
 
